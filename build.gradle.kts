0c5ee069d1e53ba0f350dcef4959cbf6c501f2f0
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2023-03-18 01:40:59 +0900

    add openai

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.apache.logging.log4j:log4j-api:2.13.3")
-    implementation("org.apache.logging.log4j:log4j-core:2.17.1")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.13.3")
+    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
     implementation("dev.kord:kord-core:0.8.0-M17")
+    implementation("com.aallam.openai:openai-client:3.1.0")
     implementation("com.mattmalec:Pterodactyl4J:2.BETA_140")
     implementation(kotlin("stdlib-jdk8"))
 }
@@ -41,10 +42,12 @@ tasks {
 kotlin {
     jvmToolchain(18)
 }
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
+
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
